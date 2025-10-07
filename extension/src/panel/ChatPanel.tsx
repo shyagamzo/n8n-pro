b791246007f1b643e1ed8df7cfc55901aef8f517
Shy Agam <us@bespunky.io>
2025-10-07 11:20:40 +0000

    🎨 Lint: remove unused useRef import from ChatPanel

--- extension/src/panel/ChatPanel.tsx
+++ extension/src/panel/ChatPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import Panel from '../lib/components/Panel'
 import Button from '../lib/components/Button'
 import Input from '../lib/components/Input'
